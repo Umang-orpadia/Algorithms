16c976c70202e06ee44ab48efbc145a1cc4bc8a4
Umang Orpadia <111333818+Umang-orpadia@users.noreply.github.com>
2024-09-15 22:47:16 +0530

    Delete Algo_all_practicals directory

--- Algo_all_practicals/sum_of_elements_in_array.py
+++
@@ -1,8 +0,0 @@
-## practical 1(a)
-lst=[]
-num=int(input("Enter the size of array : "))
-print("Enter array element")
-for n in range(num):
-   number=int(input())
-   lst.append(number)
-print("sum:",sum(lst))
